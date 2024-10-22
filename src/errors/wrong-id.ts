2a2c61aa2bbea3daf443e846c4d0ac32d72ea470
wleklinskimateusz <wleklinskimateusz05@gmail.com>
2024-10-22 10:09:46 +0200

    fix: format error file

--- src/errors/wrong-id.ts
+++ src/errors/wrong-id.ts
@@ -1,8 +1,8 @@
 export class WrongId extends Error {
-    constructor(cause?: unknown) {
-        super("Failed to parse the id");
-        if (cause) {
-            this.cause = cause;
-        }
+  constructor(cause?: unknown) {
+    super("Failed to parse the id");
+    if (cause) {
+      this.cause = cause;
     }
+  }
 }
